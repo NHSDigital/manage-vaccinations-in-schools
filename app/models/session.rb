5a23f2891a80550386327e8f1ba0830081df6630
Thomas Leese <thomas@leese.io>
2026-03-11 19:30:47 +0000

    Refactor `sync_location_programme_year_groups!`
    This refactors the method to accept `programme_types` as that's all the
    method needs.
    
    Jira-Issue: MAV-3187

--- app/models/session.rb
+++ app/models/session.rb
@@ -266,7 +266,7 @@ class Session < ApplicationRecord
     ).count
   end
 
-  def sync_location_programme_year_groups!(programmes:)
+  def sync_location_programme_year_groups!(programme_types:)
     location_programme_year_groups =
       Location::ProgrammeYearGroup
         .joins(:location_year_group)
@@ -275,7 +275,7 @@ class Session < ApplicationRecord
             location_id:,
             academic_year:
           },
-          programme_type: programmes.map(&:type)
+          programme_type: programme_types
         )
         .pluck(:programme_type, :"location_year_group.value")
 
