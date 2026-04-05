fc30da6bccdf77cbf503aa2e3869f593dc5423e8
Jake Benilov <benilov@gmail.com>
2026-04-05 08:50:50 +0100

    Add VaccinationRecordsHelper date formatting methods
    Add vaccination_record_date and vaccination_record_today_or_date
    for formatting vaccination dates in comms templates.
    
    Jira-Issue: MAV-5749

--- app/helpers/vaccination_records_helper.rb
+++ app/helpers/vaccination_records_helper.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 module VaccinationRecordsHelper
+  def vaccination_record_date(vaccination_record)
+    vaccination_record.performed_at.to_date.to_fs(:uk_short)
+  end
+
+  def vaccination_record_today_or_date(vaccination_record)
+    if vaccination_record.performed_at.today?
+      "today"
+    else
+      "on #{vaccination_record.performed_at.to_date.to_fs(:long)}"
+    end
+  end
+
   def vaccination_record_location(vaccination_record)
     vaccination_record.location_name.presence ||
       vaccination_record.location&.name || "Unknown"
